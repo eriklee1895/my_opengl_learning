56a2cfebfff5ce9c4957f8511e8168fe0d7287b0
liyuheng.erik <liyuheng.erik@bytedance.com>
2022-12-06 14:37:46 +0800

    macos开发环境搭建

---
+++ inc.h
@@ -0,0 +1,12 @@
+//
+// Created by bytedance on 2022/12/6.
+//
+
+#ifndef OPENGL_LEARNING_INC_H
+#define OPENGL_LEARNING_INC_H
+
+int draw_shape();
+
+int draw_simple_triangle();
+
+#endif //OPENGL_LEARNING_INC_H
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "inc.h"
+
+using namespace std;
+
+int main() {
+    draw_shape();
+//    draw_simple_triangle();
+}
